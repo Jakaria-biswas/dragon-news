e85bd765437667e013609f12bc2b3ad5a5736eb4
Jakaria Biswas <contactjakaria95@gmail.com>
2023-07-19 14:25:15 +0600

    last commit after the deploy client side

--- src/Components/Sheard/LeftSideNav/LeftSideNav.js
+++ src/Components/Sheard/LeftSideNav/LeftSideNav.js
@@ -7,7 +7,7 @@ const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:5000/news-category")
+        fetch("https://dragon-news-server-rho-wine.vercel.app/news-category")
             .then(res => res.json())
             .then(data => setCategories(data))
     }, []);
